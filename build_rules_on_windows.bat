c045fe7f77e841aeb18facdfa78b85111f28290f
ChristianH99 <haeussler.chr@icloud.com>
2023-12-17 20:48:44 +0100

    Added check if dos2unix is already installed and install it if necessary

--- build_rules_on_windows.bat
+++ build_rules_on_windows.bat
@@ -11,6 +11,15 @@ set REPO_PATH=%REPO_PATH:C:=/mnt/c%
 
 echo Running commands in WSL...
 
+REM Check if dos2unix is installed in WSL
+wsl dos2unix --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing dos2unix...
+    REM Add installation command for dos2unix here
+    wsl sudo apt-get update
+    wsl sudo apt-get install dos2unix
+)
+
 REM Create temporary copies of .sh scripts with Linux-style line endings using wsl cp
 wsl cp %REPO_PATH%/.ci/adoc-to-tex.sh %REPO_PATH%/.ci/adoc-to-tex-temp.sh
 wsl cp %REPO_PATH%/.ci/tex-to-pdf.sh %REPO_PATH%/.ci/tex-to-pdf-temp.sh
@@ -20,6 +29,7 @@ wsl dos2unix %REPO_PATH%/.ci/adoc-to-tex-temp.sh
 wsl dos2unix %REPO_PATH%/.ci/tex-to-pdf-temp.sh
 
 REM Run commands in WSL with sudo for Docker
+@echo Building rules
 wsl sudo docker run -v %REPO_PATH%:/documents asciidoctor/docker-asciidoctor .ci/adoc-to-tex-temp.sh onstage_rules
 wsl sudo docker run -v %REPO_PATH%:/documents mrshu/texlive-dblatex .ci/tex-to-pdf-temp.sh onstage_rules
 
