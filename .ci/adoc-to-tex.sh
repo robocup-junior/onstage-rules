31e2cecce16e42aa8efda71efa645d4dd8854f4d
ChristianH99 <haeussler.chr@icloud.com>
2025-02-23 18:45:41 +0100

    Adjusted build scripts to build more than one file from scoresheets folder
    Build of rules still untested
    Combination of Scoresheets into one PDF file still missing

--- .ci/adoc-to-tex.sh
+++ .ci/adoc-to-tex.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-mkdir -p tmp rules_documents
 
 set -euo pipefail
 
-OUTPUT_FILE=$1
+INPUT_DIR=$1
+OUTPUT_FOLDER=$(basename "$INPUT_DIR")
+rm -rf "rules_documents/$OUTPUT_FOLDER/"
+mkdir -p tmp "rules_documents/$OUTPUT_FOLDER/"
 
-cp $OUTPUT_FILE/rules.adoc tmp/$OUTPUT_FILE.adoc
+for FILE in "$INPUT_DIR"/*.adoc; do
+    FILE_NAME=$(basename "$FILE" .adoc)
+    
+    cp "$FILE" "tmp/$FILE_NAME.adoc"
+    
+    cd tmp
+    
+    cp "$FILE_NAME.adoc" "_$FILE_NAME.adoc"
+    python3 ../.ci/criticmarkup_to_adoc.py "_$FILE_NAME.adoc" > "$FILE_NAME.adoc"
+    
+    asciidoctor "$FILE_NAME.adoc"
+    asciidoctor -b docbook "$FILE_NAME.adoc"
+    
+    NEW_FILE_NAME="$FILE_NAME"
+    
+    if [[ "$FILE_NAME" =~ ^[0-9]{2}_(.*) ]]; then
+        NEW_FILE_NAME="${BASH_REMATCH[1]}"
+    fi
 
-cd tmp
+    mv "$FILE_NAME.html" "../rules_documents/$OUTPUT_FOLDER/$NEW_FILE_NAME.html"
+    
+    cd ..
 
-cp $OUTPUT_FILE.adoc _$OUTPUT_FILE.adoc
-python3 ../.ci/criticmarkup_to_adoc.py _$OUTPUT_FILE.adoc > $OUTPUT_FILE.adoc
-
-asciidoctor $OUTPUT_FILE.adoc
-asciidoctor -b docbook $OUTPUT_FILE.adoc
-
-mv $OUTPUT_FILE.html ../rules_documents/
+done
--- .ci/tex-to-pdf.sh
+++ .ci/tex-to-pdf.sh
@@ -1,25 +1,58 @@
 #!/bin/bash
 set -euo pipefail
 
-OUTPUT_FILE=$1
+INPUT_DIR=$1
+OUTPUT_FOLDER=$(basename "$INPUT_DIR")
 
 cp manual.sty tmp/
 cp -r media tmp/
 cd tmp
 
 # Apply custom styling to DocBook
-dblatex -T db2latex $OUTPUT_FILE.xml -t tex --texstyle=./manual.sty -p ../custom.xsl
+for FILE in *.xml; do
+    dblatex -T db2latex "$FILE" -t tex --texstyle=./manual.sty -p ../custom.xsl
+done
 
 # Go through the generated .tex output, find the place where the preamble ends
 # (marked by the \mainmatter command) and create a file without it.
-cat $OUTPUT_FILE.tex | awk 'f;/\\mainmatter/{f=1}'  > $OUTPUT_FILE"_without_preamble.tex"
-# Concat the standardized preamble with the "without_preamble" version of the file
-cat "../preamble.tex" "../"$OUTPUT_FILE"/header_footer.tex" "../"$OUTPUT_FILE"/title.tex" "../committee_list.tex" $OUTPUT_FILE"_without_preamble.tex" > $OUTPUT_FILE.tex
-texliveonfly $OUTPUT_FILE.tex
-pdflatex $OUTPUT_FILE.tex
-pdflatex $OUTPUT_FILE.tex
+for FILE in *.tex; do
+    FILE_NAME=$(basename "$FILE" .tex)
+    
+    cat "$FILE" | awk 'f;/\\mainmatter/{f=1}'  > "${FILE_NAME}_without_preamble.tex"
 
-mv $OUTPUT_FILE.pdf ../rules_documents/
+    ORIGINAL_TITLE_LINE=$(grep -m 1 '^\\title' "$FILE")
+    TITLE_LINE=$(echo "$ORIGINAL_TITLE_LINE" | sed 's/\\title{\(.*\)}/\\title{\\vspace{-5ex}\1\\vspace{-9ex}}/')
+
+    # Concat the standardized preamble with the "without_preamble" version of the file + additional content
+    if [[ "$FILE_NAME" == 00_* ]]; then
+        # For file starting with "00_", include committee list
+        cat "../preamble.tex" \
+        "../${INPUT_DIR}/header_footer.tex" \
+        <(echo "$TITLE_LINE") \
+        <(echo "\maketitle") \
+        "../committee_list.tex" \
+        "${FILE_NAME}_without_preamble.tex" > "$FILE_NAME.tex"
+    else
+        # For other files, skip committee list
+        cat "../preamble.tex" \
+        "../${INPUT_DIR}/header_footer.tex" \
+        <(echo "$TITLE_LINE") \
+        <(echo "\maketitle") \
+        "${FILE_NAME}_without_preamble.tex" > "$FILE_NAME.tex"
+    fi
+
+    texliveonfly $FILE_NAME.tex
+    pdflatex $FILE_NAME.tex
+    pdflatex $FILE_NAME.tex
+
+    NEW_FILE_NAME="$FILE_NAME"
+    
+    if [[ "$FILE_NAME" =~ ^[0-9]{2}_(.*) ]]; then
+        NEW_FILE_NAME="${BASH_REMATCH[1]}"
+    fi
+
+    cp $FILE_NAME.pdf "../rules_documents/$OUTPUT_FOLDER/$NEW_FILE_NAME.pdf"
+done
 
 cd ..
 rm -r tmp
